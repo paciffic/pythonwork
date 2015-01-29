a9fe50b2faced3945bc63a1fc1a2a9e992f29a53
hwhan <hwhan@paciffic>
2015-01-29 16:32:35 +0900

    two

--- javis/javis/views.py
+++ javis/javis/views.py
@@ -1,11 +1,12 @@
+from django.template import Template, Context
 from django.http import HttpResponse
 import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = ""
-    html = html + "<html><head><title>django test page</title></head>"
-    html = html + "<body><body><h1>It is now %s.</h1></body></html>" % now
+    t =  Template("<html><head><title>django test page</title></head><body><body><h1>It is {{ current_date}}.</h1></body></html>")
+    html = t.render(Context({'current_date':now}))              
+    
     return HttpResponse(html)
 
     
